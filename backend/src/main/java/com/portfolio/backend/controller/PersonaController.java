0bf84e1666ea53da2a1a29269f4714e20210dbaa
Pablo Ilari <ilari.pablo@gmail.com>
2022-08-19 21:24:07 -0300

    Backend inicial con manejo de Persona

---
+++ backend/src/main/java/com/portfolio/backend/controller/PersonaController.java
@@ -0,0 +1,61 @@
+package com.portfolio.backend.controller;
+
+import com.portfolio.backend.entity.Persona;
+import com.portfolio.backend.pinterface.IPersonaService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonaController {
+    @Autowired IPersonaService ipersonaService;
+    
+    
+    @GetMapping("/personas/traer")
+    public List<Persona> getPersona(){
+        return ipersonaService.getPersona();
+    }
+    
+    @PostMapping("/personas/crear")
+    public String createPersona(@RequestBody Persona persona) {
+        ipersonaService.savePersona(persona);
+        return "La persona fue creada correctamente";
+    }
+    
+    @DeleteMapping("/personas/borrar/(id)")
+    public String deletePersona(@PathVariable Long id) {
+        ipersonaService.deletePersona(id);
+        return "La persona fue borrada correctamente";
+    }
+    
+    
+    // URL: PUERTO/personas/editar/id/nombre&apellido&posicion...
+    @PutMapping("/personas/editar/(id)")
+    public Persona editPersona (@PathVariable Long id,
+                               @RequestParam("nombre") String nuevoNombre,
+                               @RequestParam("apellido") String nuevoApellido,
+                               @RequestParam("posicion") String nuevoPosicion,
+                               @RequestParam("about") String nuevoAbout,
+                               @RequestParam("url_img") String nuevoUrl,
+                               @RequestParam("url_back_img") String nuevoURL_back_img) {
+        
+        Persona persona = ipersonaService.findPersona(id);
+        
+        persona.setNombre(nuevoNombre);
+        persona.setApellido(nuevoApellido);
+        persona.setPosicion(nuevoPosicion);
+        persona.setAbout(nuevoAbout);
+        persona.setUrl_img(nuevoUrl);
+        persona.setUrl_back_img(nuevoURL_back_img);
+        
+        ipersonaService.savePersona(persona);
+        return persona;
+    }        
+}
---
+++ backend/src/main/java/com/portfolio/backend/entity/Persona.java
@@ -0,0 +1,43 @@
+package com.portfolio.backend.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@Entity
+public class Persona {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @NotNull
+    @Size(min = 1, max = 45, message = "No cumple con regla de longitud")
+    private String nombre;
+        
+    @NotNull
+    @Size(min = 1, max = 45, message = "No cumple con regla de longitud")
+    private String apellido;
+    
+    @NotNull
+    @Size(min = 1, max = 45, message = "No cumple con regla de longitud")
+    private String posicion;
+    
+    @NotNull
+    @Size(min = 1, max = 1000, message = "No cumple con regla de longitud")  
+    private String about;
+    
+    @NotNull
+    @Size(min = 1, max = 255, message = "No cumple con regla de longitud")   
+    private String url_img; 
+    
+    @NotNull
+    @Size(min = 1, max = 255, message = "No cumple con regla de longitud")  
+    private String url_back_img;
+   
+}
