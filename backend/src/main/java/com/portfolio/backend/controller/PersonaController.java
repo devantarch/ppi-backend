22f0aee6e41f76cfe3e65d58ee465543a80b38a1
Pablo Ilari <ilari.pablo@gmail.com>
2022-08-28 17:19:01 -0300

    Modulo de Login via JWT completado

--- backend/src/main/java/com/portfolio/backend/controller/PersonaController.java
+++ backend/src/main/java/com/portfolio/backend/controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.portfolio.backend.entity.Persona;
 import com.portfolio.backend.pinterface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,12 +24,14 @@ public class PersonaController {
         return ipersonaService.getPersona();
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/personas/crear")
     public String createPersona(@RequestBody Persona persona) {
         ipersonaService.savePersona(persona);
         return "La persona fue creada correctamente";
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/personas/borrar/{id}")
     public String deletePersona(@PathVariable Long id) {
         ipersonaService.deletePersona(id);
@@ -37,6 +40,7 @@ public class PersonaController {
     
     
     // URL: PUERTO/personas/editar/id/nombre&apellido&posicion...
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/personas/editar/{id}")
     public Persona editPersona (@PathVariable Long id,
                                @RequestParam("nombre") String nuevoNombre,
